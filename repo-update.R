a3227d3164515f0228b2feee6719657fab52da8f
timelyportfolio <kent.russell@timelyportfolio.com>
2023-04-23 18:38:40 -0500

    Update repo-update.R
    change `zipball` to `tarball`

--- repo-update.R
+++ repo-update.R
@@ -114,7 +114,7 @@ for (pkg in packages) {
 
       make_remote_tarball(
         remote_info[["package"]],
-        remote_info[["sources"]][[1]][[1]],
+        grep(x=remote_info[["sources"]][[1]][[1]],pattern="zipball",replacement="tarball"),
         remote_target
       )
     }
